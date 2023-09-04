ba79e91135f99bc40d07327c26fbef3fe3baac2b
fireship <fireship@wtf.fireship>
2023-09-04 17:43:47 +0500

    deleted: unused files

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,13 +1,4 @@
 import navbarLinks from "./navbarLinks.json";
 import adminNavbarLinks from "./adminNavbarLinks.json";
-import socialNetworks from "./socialNetworks.json";
-import specifications from "./specifications.json";
-import { skills } from "./skills";
 
-export {
-  navbarLinks,
-  adminNavbarLinks,
-  socialNetworks,
-  specifications,
-  skills,
-};
+export { navbarLinks, adminNavbarLinks };
