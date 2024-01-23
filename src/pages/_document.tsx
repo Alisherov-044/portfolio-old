0ac02602d420f4c89057ba6d07acd791d9b7ea7f
unknown <alisherov044@gmail.com>
2024-01-23 14:49:37 +0500

    fix: fixed site title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,18 @@
+import { Metadata } from "next";
 import { Html, Head, Main, NextScript } from "next/document";
+
+export const metadata: Metadata = {
+  title: "Alisherov Sadulla",
+  description: "Portfolio of Alisherov Sadulla",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Alisherov Sadulla</title>
-        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
